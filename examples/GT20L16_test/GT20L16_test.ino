2f6e4b20fd7d1ebe1191a93c550486dfb7259412
reeedstudio <luweicong@qq.com>
2013-07-03 22:53:30 +0800

    GT test fine. char & chinese

--- examples/GT20L16_test/GT20L16_test.ino
+++ examples/GT20L16_test/GT20L16_test.ino
@@ -11,6 +11,41 @@
 
 unsigned char matrix[32];
 
+void dispMatrix(int len, unsigned char *Matrix)
+{
+
+    for(int k = 0; k<2; k++)
+    {
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<(len/2); j++)
+            {
+
+                if(Matrix[j+(len/2)*k] & (0x01<<(7-i)))
+                {
+                    //Serial.print("**");
+                    Serial.write(random('A', 'Z'));         // dandom...fuck
+                    Serial.write(random('A', 'Z'));         // ...
+                }
+                else
+                {
+                    Serial.print("  ");
+                }
+            }
+            Serial.println();
+        }
+    }
+}
+
+void dispMatrixs(int len, unsigned int *unicode)
+{
+    for(int i=0; i<len; i++)
+    {
+        int dtaLen = GT20L16.getMatrixUnicode(unicode[i], matrix);
+        dispMatrix(dtaLen, matrix);
+    }
+}
+
 void setup()
 {
     Serial.begin(38400);
@@ -18,29 +53,8 @@ void setup()
     EPAPER.init_io();
     GT20L16.begin(PINCSGT);
 
-    int dtaLen = GT20L16.getMatrixUnicode('S', matrix);
-    
-    cout << "dtaLen = " << dtaLen << endl;
-
-    for(int k = 0; k<2; k++)
-    {
-        for(int i=0; i<8; i++)
-        {
-            for(int j=0; j<(dtaLen/2); j++)
-            {
-
-                if(matrix[j+(dtaLen/2)*k] & (0x01<<(7-i)))
-                {
-                    cout << "**";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            cout << endl;
-        }
-    }
+    unsigned int utest[3] = {'2', 'b', 'k'};
+    dispMatrixs(3, utest);
 
     
     cout << endl;
